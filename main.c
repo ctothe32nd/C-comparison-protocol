d2d4497b41faad766e538097597df0927aa21cf4
ctothe32nd <5018083@g.highlineschools.org>
2025-03-10 15:18:11 -0700

    for windows systems

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <windows.h>
 
 
 int main(){
@@ -25,8 +25,7 @@ int main(){
     //Compare the strings, %d shows the answer is a number
     printf("%d", strcmp(mystr, mystr2));
 
-    //Sleep for 10 seconds (should probably be adjusted)
-    sleep(10); //The 10 means 10 seconds
-
+    Sleep(10000);//10000 milliseconds, 10 seconds
+    
     return 0;
 }
